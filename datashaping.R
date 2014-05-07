df1af76e6a85d298507a014bb14a464369f1be3d
Michael Tsardakas <mtsarduckas@gmail.com>
2014-05-07 15:16:30 +0100

    Added datashaping.R

--- datashaping.R
+++ datashaping.R
@@ -1,37 +1,41 @@
 library("plyr", lib.loc="/Library/Frameworks/R.framework/Versions/3.0/Resources/library")
+library("reshape", lib.loc="/Library/Frameworks/R.framework/Versions/3.0/Resources/library")
 library("ggplot2", lib.loc="/Library/Frameworks/R.framework/Versions/3.0/Resources/library")
-setwd("~/Dropbox/CrimeData/Merseyside")
+library("caret", lib.loc="/Library/Frameworks/R.framework/Versions/3.0/Resources/library")
+library("Hmisc", lib.loc="/Library/Frameworks/R.framework/Versions/3.0/Resources/library")
+library("xtable", lib.loc="/Library/Frameworks/R.framework/Versions/3.0/Resources/library")
 
-filenames<-list.files("~/Dropbox/CrimeData/Merseyside")
+# setwd("~/Dropbox/CrimeData/Manchester")
+filenames<-list.files("~/Dropbox/CrimeData/Manchester",full.names=TRUE)
 dataset <- ldply(filenames, read.csv, header=TRUE)
-Southampton<-dataset
-Southampton<-Southampton[Southampton$Crime.type!="Drugs",]
- Southampton<-Southampton[Southampton$Crime.type!="Other crime",]
- Southampton<-Southampton[Southampton$Crime.type!="Anti-social behaviour",]
-Southampton$Crime.type<-factor(Southampton$Crime.type)
- crime.f<-factor(Southampton$Crime.type)
- dummies <- dummyVars(~Crime.type,data = Southampton)
- dummies2<-predict(dummies, newdata=Southampton)
+Manchester<-dataset
+Manchester<-Manchester[Manchester$Crime.type!="Drugs",]
+ Manchester<-Manchester[Manchester$Crime.type!="Other crime",]
+ Manchester<-Manchester[Manchester$Crime.type!="Anti-social behaviour",]
+Manchester$Crime.type<-factor(Manchester$Crime.type)
+ crime.f<-factor(Manchester$Crime.type)
+ dummies <- dummyVars(~Crime.type,data = Manchester)
+ dummies2<-predict(dummies, newdata=Manchester)
  dummies2<-as.data.frame(dummies2)
-Southampton$Serious<-dummies2$Crime.type.Burglary + dummies2$Crime.type.Robbery + dummies2$"Crime.type.Criminal damage and arson" + dummies2$"Crime.type.Violent crime" + dummies2$"Crime.type.Violence and sexual offences"
- Southampton$Minor<-  dummies2$Crime.type.Shoplifting + dummies2$"Crime.type.Vehicle crime" + dummies2$"Crime.type.Other theft" + dummies2$"Crime.type.Public disorder and weapons" + dummies2$"Crime.type.Theft from the person" + dummies2$"Crime.type.Public order" + dummies2$"Crime.type.Possession of weapons" + dummies2$"Crime.type.Bicycle theft"
-aggdata<-aggregate(Serious ~ Month, Southampton, FUN=sum)
- aggdata2<-aggregate(Minor ~ Month, Southampton, FUN=sum)
-SouthamptonMerged<-data.frame(Month=aggdata$Month, Serious=aggdata$Serious, Minor=aggdata2$Minor, Location="Southampton")
-SouthamptonMerged<-SouthamptonMerged[-(1:9),]
-for(i in 1:27)
-  SouthamptonMerged$DS[i]<-SouthamptonMerged$Serious[i+1]-SouthamptonMerged$Serious[i] 
-for(i in 1:27)
-  SouthamptonMerged$DM[i]<-SouthamptonMerged$Minor[i+1]-SouthamptonMerged$Minor[i]
-Southampton1<-data.frame(Month=SouthamptonMerged$Month, Amount=SouthamptonMerged$Serious, isin="Serious", Location="Southampton")
-Southampton2<-data.frame(Month=SouthamptonMerged$Month, Amount=SouthamptonMerged$Minor, isin="Minor", Location="Southampton")
-Southampton3<-rbind(Southampton1, Southampton2)
+Manchester$Serious<-dummies2$Crime.type.Burglary + dummies2$Crime.type.Robbery + dummies2$"Crime.type.Criminal damage and arson" + dummies2$"Crime.type.Violent crime" + dummies2$"Crime.type.Violence and sexual offences"
+ Manchester$Minor<-  dummies2$Crime.type.Shoplifting + dummies2$"Crime.type.Vehicle crime" + dummies2$"Crime.type.Other theft" + dummies2$"Crime.type.Public disorder and weapons" + dummies2$"Crime.type.Theft from the person" + dummies2$"Crime.type.Public order" + dummies2$"Crime.type.Possession of weapons" + dummies2$"Crime.type.Bicycle theft"
+aggdata<-aggregate(Serious ~ Month, Manchester, FUN=sum)
+ aggdata2<-aggregate(Minor ~ Month, Manchester, FUN=sum)
+ManchesterMerged<-data.frame(Month=aggdata$Month, Serious=aggdata$Serious, Minor=aggdata2$Minor, Location="Manchester")
+ManchesterMerged<-ManchesterMerged[-(1:9),]
+for(i in 1:(length(filenames)-10))
+  ManchesterMerged$DS[i]<-ManchesterMerged$Serious[i+1]-ManchesterMerged$Serious[i] 
+for(i in 1:(length(filenames)-10))
+  ManchesterMerged$DM[i]<-ManchesterMerged$Minor[i+1]-ManchesterMerged$Minor[i]
+Manchester1<-data.frame(Month=ManchesterMerged$Month, Amount=ManchesterMerged$Serious, isin="Serious", Location="Manchester")
+Manchester2<-data.frame(Month=ManchesterMerged$Month, Amount=ManchesterMerged$Minor, isin="Minor", Location="Manchester")
+Manchester3<-rbind(Manchester1, Manchester2)
 
-plotSouthampton<- ggplot(Southampton3, aes(Month,Amount,group=isin, color=isin)) + geom_line() +xlab("Months") + ylab("Criminal activites") + ggtitle("Southampton") + geom_vline(xintercept=c(7,13,19,25), linetype="dotted")  + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + scale_color_manual(values=c("red", "orange"),name="Crime type")
+plotManchester<- ggplot(Manchester3, aes(Month,Amount,group=isin, color=isin)) + geom_line() +xlab("Months") + ylab("Criminal activites") + ggtitle("Manchester") + geom_vline(xintercept=c(7,13,19,25), linetype="dotted")  + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + scale_color_manual(values=c("red", "orange"),name="Crime type")
 
- print(plotSouthampton)
- ggsave("~/Dropbox/CrimeData/Pics/Southampton4.png", height=6.44, width=8.00,units=c("in"), dpi=100)
- dev.off()
-  print(plotSouthampton)
- ggsave("~/Dropbox/CrimeData/Pics/Southampton4.eps", height=6.44, width=8.00, units=c("in"), dpi=100)
+ print(plotManchester)
+#  ggsave("~/Dropbox/CrimeData/Pics/test.png", height=6.44, width=8.00,units=c("in"), dpi=100)
+#  dev.off()
+#   print(plotManchester)
+#  ggsave("~/Dropbox/CrimeData/Pics/test.eps", height=6.44, width=8.00, units=c("in"), dpi=100)
 
